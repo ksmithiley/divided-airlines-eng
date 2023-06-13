473d7bcf89ada7b9f81ddbacbb0cd30e92aefd52
KaeleeNaevah <136414407+KaeleeNaevah@users.noreply.github.com>
2023-06-12 20:40:36 -0700

    Create accounting-importer.py

---
+++ accounting-importer.py
@@ -0,0 +1,8 @@
+# Excel spreadsheet for reference: https://docs.google.com/spreadsheets/d/1gd-cTNXy8ugxaSBOb6Xxhdr1m4tLfV56z3JolLnQNto/edit?usp=sharing (delete later)
+
+import boto3
+import pandas as pd
+
+# Fake S3 bucket information
+bucket_name = 'temp-s3-bucket'
+file_name = 'temp.xlsx'
